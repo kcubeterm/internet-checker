9838a7847b311e230d7c4755069be9953aafa890
Krishna Kanhaiya <50498359+kcubeterm@users.noreply.github.com>
2019-11-25 12:56:52 +0530

    Add files via upload

---
+++ tmate-setup.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/bash
+pkg update -y
+pkg upgrade -y
+chmod +x ~/internet-checker/tmate-boot.sh
+chmod +x ~/internet-checker/internet-checker.sh
+cp ~/internet-checker/tmate-boot.sh $PREFIX/bin
+cp ~/internet-checker/internet-checker.sh $PREFIX/bin
+
+rm -r  ~/.termux/boot
+mkdir ~/.termux
+mkdir ~/.termux/boot
+cd ~/.termux/boot
+cp ~/internet-checker/start-tmate ~/.termux/boot
+chmod +x ~/internet-checker/start-tmate
+echo "complete"
